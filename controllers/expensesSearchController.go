95fd7777611b7b3515a69c3708b777a467980f8f
Silas S. da Silva <silvasilas66@gmail.com>
2024-01-17 22:31:48 -0300

    Criando controlador com método de busca básico

--- controllers/expensesSearchController.go
+++ controllers/expensesSearchController.go
@@ -1,49 +1,30 @@
-// func Login(w http.ResponseWriter, r *http.Request) {
-// 	user := &models.User{}
-// 	err := json.NewDecoder(r.Body).Decode(user) 
-// 	if err != nil {
-// 		var resp = map[string]interface{}{"status": false, "message": "Invalid request"}
-// 		json.NewEncoder(w).Encode(resp)
-// 		return
-// 	}
-// 	resp := FindOne(user.Email, user.Password)
-// 	json.NewEncoder(w).Encode(resp)
-// }
+package ExpenseSearchController
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	//"github.com/silvasilas99/despesas_orcamentarias/models"
+)
 
-// func FindOne(email, password string) map[string]interface{} {
-// 	user := &models.User{}
+func get(w http.ResponseWriter, r *http.Request) {
+	URL := "http://dados.recife.pe.gov.br/api/3/action/datastore_search?resource_id=d4d8a7f0-d4be-4397-b950-f0c991438111&limit=5"
+	resp, err := http.Get(URL)
+	if err != nil {
+		log.Fatal("Não foi possivel obter os resultados da requisição.")
+	}
 
-// 	if err := db.Where("Email = ?", email).First(user).Error; err != nil {
-// 		var resp = map[string]interface{}{"status": false, "message": "Email address not found"}
-// 		return resp
-// 	}
-// 	expiresAt := time.Now().Add(time.Minute * 100000).Unix()
+	responseBody, error := io.ReadAll(resp.Body)
 
-// 	errf := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
-// 	if errf != nil && errf == bcrypt.ErrMismatchedHashAndPassword { //Password does not match!
-// 		var resp = map[string]interface{}{"status": false, "message": "Invalid login credentials. Please try again"}
-// 		return resp
-// 	}
+	if error != nil {
+		fmt.Println(error)
+	}
 
-// 	tk := &models.Token{
-// 		UserID: user.ID,
-// 		Name:   user.Name,
-// 		Email:  user.Email,
-// 		StandardClaims: &jwt.StandardClaims{
-// 			ExpiresAt: expiresAt,
-// 		},
-// 	}
+	formattedData := json.NewEncoder(w).Encode(responseBody)
+	fmt.Println("Status: ", resp.Status)
+	fmt.Println("Response body: ", formattedData)
 
-// 	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
-
-// 	tokenString, error := token.SignedString([]byte("secret"))
-// 	if error != nil {
-// 		fmt.Println(error)
-// 	}
-
-// 	var resp = map[string]interface{}{"status": false, "message": "logged in"}
-// 	resp["token"] = tokenString //Store the token in the response
-// 	resp["user"] = user
-// 	return resp
-// }
+	defer resp.Body.Close()
+}
